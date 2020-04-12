3edde55f7320d1dbbcb4ec10d7877f1791789597
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2020-04-12 22:34:12 +0900

    [add]assert

--- machine_learning/program/surpervised_learning/model/lightGBM.py
+++ machine_learning/program/surpervised_learning/model/lightGBM.py
@@ -3,8 +3,8 @@ import lightgbm as lgb
 
 class LgbClassifier(object):
     def __init__(self, model_params={}):
+        assert isinstance(model_params, dict)
         self.params = model_params
-
         self.build()
 
     def build(self):
