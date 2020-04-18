158017238cf82726872df53fa55c1f023a6cbaa3
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2020-04-18 22:17:55 +0900

    [add]whether use config to build lgb model

--- machine_learning/program/surpervised_learning/model/lightGBM.py
+++ machine_learning/program/surpervised_learning/model/lightGBM.py
@@ -2,9 +2,9 @@ import lightgbm as lgb
 
 
 class LgbClassifier(object):
-    def __init__(self, model_params={}):
-        assert isinstance(model_params, dict)
-        self.params = model_params
+    def __init__(self, model_params={}, use_model_param_config=False):
+        if use_model_param_config:
+            self.params = model_params
         self.build()
 
     def build(self):
