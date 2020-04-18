c78d5c86cd8aa8dc241042a1b742957cee9a5beb
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2020-04-18 23:01:27 +0900

    [add]implement fit

--- machine_learning/program/surpervised_learning/trainer/trainer.py
+++ machine_learning/program/surpervised_learning/trainer/trainer.py
@@ -2,4 +2,9 @@ class Trainer(object):
     def __init__(self, config):
         self.target =config.get('train_params', 'target_param')
 
-    def fit(self, model, train_data):
+    def fit(self, model, train_data_df):
+        y_train = train_data_df[self.target]
+        x_train = train_data_df.drop(columns=self.target)
+        model.fit(x_train, y_train)
+
+        return model
