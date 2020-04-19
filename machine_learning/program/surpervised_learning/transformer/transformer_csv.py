d1efd45c3e81b737354135e632e66d7fc5750a7f
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2020-04-19 23:57:05 +0900

    [add]def encode_category_to_ordinal_int, def encode_category_to_int

--- machine_learning/program/surpervised_learning/transformer/transformer_csv.py
+++ machine_learning/program/surpervised_learning/transformer/transformer_csv.py
@@ -22,9 +22,13 @@ class TransformerCsv(object):
         for drop_col in drop_list:
             self.data_df = self.data_df.drop(drop_col)
 
-    def encode_label(self, encode_label_list):
+    def encode_category_to_ordinal_int(self, encode_ordinal_map_dict):
+        for ordinal_categorical_param, ordinal_map in encode_ordinal_map_dict.item():
+            self.data_df[ordinal_categorical_param] = self.data_df[ordinal_categorical_param].map(ordinal_map)
 
-    def encode_category(self, categorical_list):
+    def encode_category_to_int(self, categorical_list):
+        for categorical_param in categorical_list:
+            self.data_df[categorical_param] = self.data_df[categorical_param].astype('category').cat.codes
 
     def split_train_test_data(self, test_size=0.4, shuffle=False):
         self.train_df, self.test_df = train_test_split(self.data_df, test_size=test_size, shuffle=shuffle)
