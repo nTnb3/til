5cffa1d5e620dec7317017e5140d10e4638b70b7
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2020-04-25 14:41:08 +0900

    [fix]chage self.target to model.target

--- machine_learning/program/surpervised_learning/trainer/trainer.py
+++ machine_learning/program/surpervised_learning/trainer/trainer.py
@@ -3,8 +3,8 @@ class Trainer(object):
         pass
 
     def fit(self, model, train_data_df):
-        y_train = train_data_df[self.target]
-        x_train = train_data_df.drop(columns=self.target)
+        y_train = train_data_df[model.target]
+        x_train = train_data_df.drop(columns=model.target)
         model.fit(x_train, y_train)
 
         return model
