ef1e7a53df045100285a79c93252cb1e683dc480
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2020-04-25 15:05:01 +0900

    [add]def _read_model_params

--- machine_learning/program/surpervised_learning/model/lightGBM.py
+++ machine_learning/program/surpervised_learning/model/lightGBM.py
@@ -1,16 +1,22 @@
 import lightgbm as lgb
 from sklearn.metrics import accuracy_score
+from csv import DictReader
 
 class LgbClassifier(object):
-    def __init__(self, model_params, target_col, use_model_param_config=False):
+    def __init__(self, model_params_path, target_col, use_model_param_config=False):
         self.target = target_col
         params = {}
         if use_model_param_config:
-            params = model_params
+            params = self._read_model_params(model_params_path)
         self._build(params)
 
     def _build(self, params):
         self.model = lgb.LGBMClassifier(**params)
+
+    def _read_model_params(self, model_params_path):
+        with open(model_params_path, newline='') as csvfile:
+            model_params = DictReader(csvfile)
+            return model_params
 
     def _predict(self, test_data):
         return self.model.predict(test_data)
