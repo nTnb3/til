6eefaf9eed27c1ae18c5f90523cd3056d84172b0
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2020-04-28 15:14:29 +0900

    [add]def plot_roc_curtve

--- machine_learning/program/surpervised_learning/utils/plot_result.py
+++ machine_learning/program/surpervised_learning/utils/plot_result.py
@@ -1,8 +1,23 @@
- class PlotClassificationResult(object):
-     def __init__(self):
-         raise NotImplementedError
+import matplotlib.pyplot as plt
 
 
- class PlotRegressionResult(object):
-     def __init__(self):
-         raise NotImplementedError
+class PlotClassificationResult(object):
+    def __init__(self):
+        pass
+
+    def plot_roc_curve(self, roc_tapple, plot_show=False):
+        tpr = roc_tapple[1]
+        thresholds = roc_tapple[2]
+        fpr = roc_tapple[0]
+
+        plt.plot(fpr, tpr, marker='o')
+        plt.xlabel('FPR: False positive rate')
+        plt.ylabel('TPR: True positive rate')
+        plt.grid()
+        if plot_show:
+            plt.show()
+
+
+class PlotRegressionResult(object):
+    def __init__(self):
+        raise NotImplementedError
