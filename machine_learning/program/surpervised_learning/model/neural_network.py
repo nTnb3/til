91f7690ddca3edc7794a5e7800f8f9f23976efc9
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2020-05-21 08:06:27 +0900

    [add]def init

--- machine_learning/program/surpervised_learning/model/neural_network.py
+++ machine_learning/program/surpervised_learning/model/neural_network.py
@@ -2,5 +2,12 @@ import tensorflow as tf
 
 
 class NeuralNetworkClassifier(object):
-    def __init__(self, input_layer_num, middle_layer_list, output_layer_num):
+    def __init__(self, input_layer_num=3, middle_layer_list=[16,16], output_layer_num=1):
+        self.input_layer_num = input_layer_num
+        self.middle_layer_list = middle_layer_list
+        self.output_layer_num = output_layer_num
+        self._build()
+
+    def _build(self):
+
 
