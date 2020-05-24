e372d86eada0fc01ca6b0d2ec31b1717266aa42b
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2020-05-25 08:27:05 +0900

    [add]def fit

--- machine_learning/program/surpervised_learning/trainer/trainer.py
+++ machine_learning/program/surpervised_learning/trainer/trainer.py
@@ -11,11 +11,23 @@ class Trainer(object):
 
 
 class TrainerNeuralNetwork(Trainer):
-    def __init__(self, optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy']):
+    def __init__(self):
         super().__init__()
-        self.optimizer = optimizer
-        self.loss = loss
-        self.metrics = metrics
 
-    def fit(self, model_class, train_data_df):
-        model_class.model.compile(optimizer=self.optimizer, loss=self.loss, metrics=self.metrics)
+    def fit(self,
+            model_class,
+            train_data_df,
+            epoch=20,
+            batch_size=32,
+            optimizer='adam',
+            loss='sparse_categorical_crossentropy',
+            metrics=['accuracy'],
+            validation_split=0.2):
+
+        y_train = train_data_df[model_class.target]
+        x_train = train_data_df.drop(columns=model_class.target)
+        model_class.model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+        model_class.history = model_class.model.fit(x_train, y_train, epoch=epoch,
+                                                    batch_size=batch_size, validation_split=validation_split)
+
+        return model_class
