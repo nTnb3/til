976cf47e387ea907fade5811bfd0bcf890bba1a8
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2021-03-24 08:30:02 +0900

    [fix]nav部分の開閉

--- web_design/cp_coding/PF/pf/js/script.js
+++ web_design/cp_coding/PF/pf/js/script.js
@@ -18,3 +18,37 @@ $(function () {
   });
 
 });
+
+$(function(){
+    $("#open").show();
+    $("#close").hide();
+  // アイコンをクリック
+  $("#open").click(function(){
+  // ulメニューを開閉する
+  $("#navi").slideToggle();
+    $("#open").hide();
+    $("#close").show();
+  });
+    
+  // アイコンをクリック
+  $("#close").click(function(){
+  // ulメニューを開閉する
+  $("#navi").slideToggle();
+    $("#open").show();
+    $("#close").hide();
+  });
+
+});
+
+$(function(){
+	// メニューをクリック
+	$("a[href*=#]:not([href=#])").click(function(){
+		// 移動先のコンテンツ位置を取得
+		var target = $($(this).attr("href")).offset().top;
+		// 50px減らす
+		target -= 50;
+		// 各コンテンツへスクロール
+		$("html, body").animate({scrollTop: target}, 500);
+		return false;
+	});
+});
