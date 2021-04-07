ec23b85ee07c98a56a74e43b177c00ae8e61130f
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2021-04-08 08:01:41 +0900

    [fix]コメント文を取得内容ごとに変更

--- web_op/sample_scraping.py
+++ web_op/sample_scraping.py
@@ -3,15 +3,20 @@ from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
 # Webページを取得して解析する
-url = "https://jp.louisvuitton.com/jpn-jp/products/pochette-melanie-mm-monogram-empreinte-nvprod2020033v#M68707"
-
+# url = "https://jp.louisvuitton.com/jpn-jp/products/pochette-melanie-mm-monogram-empreinte-nvprod2020033v#M68707"
+url = "https://jp.louisvuitton.com/jpn-jp/products/placed-graphic-shirt-nvprod2550073v"
 # セッション開始
 session = HTMLSession()
 r = session.get(url)
 
-# スクレイピング(製品名の取得)
+# スクレイピング
+# 製品名をclassから取得
 product_title = r.html.find('.lv-product__title')
+# 製品仕様をidから取得
 read_more = r.html.find('#read-more')
+# サイズ一覧をclassから取得
+size = r.html.find('.lv-product-panel-list__item-name')
+
 # read-more = r.html.find('#read-more > p ')
 
 print(" - 製品名:")
@@ -22,4 +27,10 @@ print("\n")
 
 print(" - 製品仕様：")
 for e in read_more:
+    print(e.text)
+
+print("\n")
+
+print(" - サイズ：")
+for e in size:
     print(e.text)
