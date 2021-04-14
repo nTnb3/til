3d772d3895a828e5339e083d554cf742f2f95429
Taguchi Naoki <taguchinaoki@TaguchinoMac-mini.local>
2021-04-14 19:27:57 +0900

    [add]データ取得クラスの定義

--- web_op/exhibition_page_create_bym/bym_exhibition_page_creater.py
+++ web_op/exhibition_page_create_bym/bym_exhibition_page_creater.py
@@ -1,4 +1,7 @@
+import datetime
+
 from selenium import webdriver
+import chromedriver_binary
 
 from .bym_page_data_collector import BymPageDataCollector
 from .lv_page_data_collector import LvPageDataCollector
@@ -7,10 +10,15 @@ from create_ja_area_list import prefecture
 
 
 class BymExhibitionPageCreater(object):
-    def __init__(self, exhibition_url, ref_bym_url, lv_url, title_msg):
+    def __init__(self, ref_bym_url, lv_url, title_msg, exhbt_no, exhibition_url):
         self.title_msg = title_msg
-        self.bym_data_collector = BymPageDataCollector(ref_bym_url)
+
+        dt_now = datetime.datetime.now()
+        today = dt_now.strftime('%m-%d')
+        self.bym_data_collector = BymPageDataCollector(ref_bym_url, today, exhbt_no)
+        bym_extract_data = self.bym_data_collector.data_dict
         self.lv_data_collector = LvPageDataCollector(lv_url)
+        lv_extract_data = self.lv_data_collector.data_dict
 
     def _activate_browser(self, exhibition_url):
         # ブラウザを起動する
