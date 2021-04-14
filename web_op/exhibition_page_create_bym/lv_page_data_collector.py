4547bd810327ffd3003db2c940a5f82cfadaba21
unknown <ntaguchi0313123@gmail.com>
2021-04-14 14:55:16 +0900

    [add]必要関数実装

--- web_op/exhibition_page_create_bym/lv_page_data_collector.py
+++ web_op/exhibition_page_create_bym/lv_page_data_collector.py
@@ -5,29 +5,68 @@ from selenium import webdriver
 
 
 class LvPageDataCollector(object):
-    def __init__(self, en_prod_url, jp_prod_tmp_url="https://jp.louisvuitton.com/jpn-jp/search/"):
+    def __init__(self, en_prod_url, is_bag, jp_prod_tmp_url="https://jp.louisvuitton.com/jpn-jp/search/"):
+        self.is_bag = is_bag
+        self._data_dict = {}
         self._ready_scraping(en_prod_url, jp_prod_tmp_url)
 
     def _ready_scraping(self, en_prod_url, jp_prod_tmp_url):
         # セッション開始
         session = HTMLSession()
         r = session.get(en_prod_url)
-        self._access_jp_page(r, session, jp_prod_tmp_url)
+        self.jp_prod_url = self._access_jp_page(r, session, jp_prod_tmp_url)
 
     def _access_jp_page(self, r, sess, jp_prod_tmp_url):
         # 外国語ページから型番をclassから取得
-        product_no = self._fetch_prod_no()
+        product_code = self._fetch_prod_code()
         # 取得した型番から日本語版ページにアクセスする
-        jp_prod_url = jp_prod_tmp_url + product_no
+        jp_prod_url = jp_prod_tmp_url + product_code
         self.r = sess.get(jp_prod_url)
+        return jp_prod_url
+
+    def _init_data_dict(self):
+        data_label = ["prod_name",
+                      "season",
+                      "theme",
+                      "tag",
+                      "palace",
+                      "price",
+                      "img_path"]
+        for label in data_label:
+            self._data_dict[label] = None
+
+    @property
+    def data_dict(self):
+        return self._data_dict
+
+    def _fetch_data(self, today, exhbt_no, save_root_path):
+        self._data_dict["prod_name"] = self._fetch_prod_name()
+        self._data_dict["prod_code"] = self._fetch_prod_code()
+        self._data_dict["prod_code_list"] = self._fetch_prod_code_list()
+        self._data_dict["prod_spec"] = self._fetch_prod_spec()
+        self._data_dict["size_list"] = self._fetch_size_list()
+        self._data_dict["size_table"] = self._fetch_size_table()
 
     def _fetch_prod_name(self):
         prod_name = self.r.html.find('.lv-product__title')
         return prod_name[0].text
 
-    def _fetch_prod_no(self):
-        product_no = self.r.html.find('.lv-product__details-sku')
-        return product_no[0].text
+    def _fetch_prod_code(self):
+        product_code = self.r.html.find('.lv-product__details-sku')
+        return product_code[0].text
+
+    def _fetch_prod_code_list(self):
+        product_code_list = []
+        if self.is_bag:
+            product_codes_e = self.r.html.find('.lv-choice')
+            for e in product_codes_e:
+                if e.text != self._data_dict["prod_name"]:
+                    product_code_list.append(e.attrs["id"])
+        else:
+            product_codes_e = self.r.html.find('.lv-product-card__name')
+            for e in product_codes_e:
+                product_code_list.append(e.attrs["id"].split('-')[-1])
+        return product_code_list
 
     def _fetch_prod_spec(self):
         prod_spec = self.r.html.find('#read-more')
@@ -48,22 +87,39 @@ class LvPageDataCollector(object):
         return color_list
 
     def _fetch_size_table(self):
-        # BeautifulSoupでhtmlをパース
-        soup = BeautifulSoup(html, "html.parser")
-        # table取得
-        table = soup.findAll("table", {"class": "lv-size-guide-table__table"})[0]
-        rows = table.findAll("tr")
-        for row in rows:
-            csvRow = []
-            for cell in row.findAll(['td', 'th']):
-                csvRow.append(cell.get_text())
-            print(csvRow)
+        self._activate_browser()
+        # サイズガイドを取得し、クリック
+        is_table = True
+        try:
+            element = self.driver.find_element_by_class_name('lv-product-size-guide__button').click()
+        except:
+            is_table = False
+
+        # 文字コードをUTF-8に変換
+        html = self.driver.page_source.encode('utf-8')
+
+        # ブラウザを閉じる
+        self._close_browser()
+
+        table_list = []
+        if is_table:
+            # BeautifulSoupでhtmlをパース
+            soup = BeautifulSoup(html, "html.parser")
+            # table取得
+            table = soup.findAll("table", {"class": "lv-size-guide-table__table"})[0]
+            rows = table.findAll("tr")
+            for row in rows:
+                csvRow = []
+                for cell in row.findAll(['td', 'th']):
+                    csvRow.append(cell.get_text())
+                table_list.append(csvRow)
+        return table_list
 
     def _activate_browser(self):
         # ブラウザを起動する
         self.driver = webdriver.Chrome()
         # ブラウザでアクセスする
-        self.driver.get(self.ref_bym_url)
+        self.driver.get(self.jp_prod_url)
 
     def _close_browser(self):
         # ブラウザを閉じる
