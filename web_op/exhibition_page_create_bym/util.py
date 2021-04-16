79216b1c7597499580708a98d0c4dcee78172758
unknown <ntaguchi0313123@gmail.com>
2021-04-16 16:13:01 +0900

    [add]画像のリサイズ機能を追加

--- web_op/exhibition_page_create_bym/util.py
+++ web_op/exhibition_page_create_bym/util.py
@@ -1,4 +1,12 @@
+from PIL import Image
+
 
 def merge_dict(dict1, dict2):
     return dict1.update(dict2)
 
+
+def resize_img(img_path, img_size_tpl=(700, 700)):
+    img = Image.open(img_path)
+
+    img_resize = img.resize(img_size_tpl)
+    img_resize.save(img_path)
